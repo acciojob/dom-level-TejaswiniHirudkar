7fa7385b8abe23215fd7c112168b0f37757f176b
Tejaswini Hirudkar <121488475+TejaswiniHirudkar@users.noreply.github.com>
2023-06-22 20:24:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+let element=document.getElementById('level');
+let level=0;
+if(element){
+	let currelement=element;
+	while(currelement.parentNode){
+		currelement=currelement.parentNode;
+		level++;
+	}
+}
+alert("The level of the element is: "+ level);
+
